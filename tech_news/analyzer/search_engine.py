2d697dddb74cc319f4f61e3379c62d3517752905
guizarolho <guimfontes7@gmail.com>
2022-06-23 19:32:32 -0300

    req 10

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -3,7 +3,7 @@ from tech_news.database import search_news
 
 # Requisito 6
 def search_by_title(title):
-    results = search_news({'title': {'$regex': title, "$options": "i"}})
+    results = search_news({'title': {'$regex': title, '$options': 'i'}})
     list = []
     for news in results:
         news_tuple = tuple([news['title'], news['url']])
@@ -23,9 +23,14 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    results = search_news({'category': {'$regex': category, '$options': 'i'}})
+    list = []
+    for news in results:
+        news_tuple = tuple([news['title'], news['url']])
+        list.append(news_tuple)
+    return list
 
 
 if __name__ == '__main__':
-    results = search_by_title("NotíCIa")
+    results = search_by_category("novidades")
     print(results)
