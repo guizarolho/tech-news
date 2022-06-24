7b500cbee0c89296c37c9c90a86a6f6ec7d01353
guizarolho <guimfontes7@gmail.com>
2022-06-24 15:34:06 -0300

    switch

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,6 +1,63 @@
+from sys import stderr
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.ratings import top_5_categories, top_5_news
+from tech_news.analyzer.search_engine import (
+    search_by_category,
+    search_by_date,
+    search_by_tag,
+    search_by_title,
+    )
+
+
+def get_news():
+    number = input('Digite quantas notícias serão buscadas:')
+    return get_tech_news(int(number))
+
+
+def get_date():
+    date = input('Digite a data no formato aaaa-mm-dd:')
+    return search_by_date(date)
+
+
+def get_title():
+    title = input('Digite o título:')
+    return search_by_title(title)
+
+
+def get_tag():
+    tag = input('Digite a tag:')
+    return search_by_tag(tag)
+
+
+def get_category():
+    category = input('Qual a categoria:')
+    return search_by_category(category)
+
+
+def end_script():
+    return 'Encerrando script'
+
+
+def switch(option):
+    opcoes = {
+        '0': get_news,
+        '1': get_title,
+        '2': get_date,
+        '3': get_tag,
+        '4': get_category,
+        '5': top_5_news,
+        '6': top_5_categories,
+        '7': end_script
+    }
+    try:
+        return opcoes.get(option, stderr)()
+    except TypeError:
+        return 'Opção inválida'
+
+
 # Requisito 12
 def analyzer_menu():
-    input("""
+    option = input("""
     Selecione uma das opções a seguir:
  0 - Popular o banco com notícias;
  1 - Buscar notícias por título;
@@ -11,4 +68,10 @@ def analyzer_menu():
  6 - Listar top 5 categorias;
  7 - Sair.
     """)
-    """Seu código deve vir aqui"""
+
+    mensagem = switch(option)
+    print(mensagem)
+
+
+if __name__ == '__main__':
+    analyzer_menu()
