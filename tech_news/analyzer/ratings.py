1e953c2d2da775eca84bc5aeeae28a59beb9af00
guizarolho <guimfontes7@gmail.com>
2022-06-24 12:01:05 -0300

    req 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from collections import Counter
 
 
 # Requisito 10
@@ -21,9 +22,20 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    results = find_news()
+
+    list = {}
+    for news in results:
+        if not list.get(news['category']):
+            list[news['category']] = 0
+        list[news['category']] += 1
+    key_value = Counter(list).most_common(5)
+    list_names = []
+    for tuple in key_value:
+        list_names.append(tuple[0])
+    return list_names
 
 
 if __name__ == '__main__':
-    content = top_5_news()
+    content = top_5_categories()
     print(content)
